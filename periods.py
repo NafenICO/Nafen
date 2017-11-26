6fb5adc4ae227f5765606789139344ace26b3275
Alex Nikolaev <amijkko@yandex.ru>
2017-11-26 13:23:44 +0300

    added periods file

---
+++ periods.py
@@ -0,0 +1,9 @@
+import  time;
+
+startA = time.time()
+periodA = 180
+startB = startA + 240
+periodB = 180
+startC = startB + 240
+periodC = 180
+print("просто копируй текст ниже бро:\n %d, %d, %d, %d, %d, %d" % (startA, periodA, startB, periodB, startC, periodC))
