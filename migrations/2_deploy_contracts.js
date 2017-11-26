a0c09a2c1791bf4780508a05cdabea91eab60697
Alex Nikolaev <amijkko@yandex.ru>
2017-11-27 02:45:19 +0300

    added deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ var PrivatePlacement = artifacts.require("./PrivatePlacement.sol");
 
 
 module.exports = function(deployer) {
-  deployer.deploy(NafenToken);
-  deployer.deploy(PrivatePlacement);
+  deployer.deploy(NafenToken).then(function() {
+  return deployer.deploy(PrivatePlacement, NafenToken.address,accounts[0],Date.now(),180,Date.now()+240,180,Date.now()+480,180);
+});
 };
