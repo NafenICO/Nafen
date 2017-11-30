65322025aea0ab3b3587377113b38428ea1f21a4
DenisKaizer <1kaizerd@gmail.com>
2017-11-30 13:39:23 +0300

    privateplacement => crowdsale

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -252,7 +252,7 @@ contract FiatContract {
   function updatedAt(uint _id) constant returns (uint);
 }
 
-contract PrivatePlacement is Ownable {
+contract Crowdsale is Ownable {
   
   using SafeMath for uint;
 
@@ -276,7 +276,7 @@ contract PrivatePlacement is Ownable {
 
   bool isUnderHardCap = true;
 
-  function PrivatePlacement(
+  function Crowdsale(
   address tokenAddress,
   address _multisig,
   uint _startA,
